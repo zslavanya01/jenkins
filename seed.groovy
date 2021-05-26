8219f72aa712220aedf01665e6601eadce5c4f14
zslavanya <lavanya.k@zelarsoft.com>
2021-05-26 16:18:39 +0530

    ..

--- seed.groovy
+++ seed.groovy
@@ -25,3 +25,91 @@ pipelineJob('CI-Pipelines/frontend-ci') {
 
     }
 }
+
+pipelineJob('CI-Pipelines/login-ci') {
+    configure { flowdefination ->
+      flowdefination << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+          'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+              'userRemoteConfigs' {
+                  'hudson.plugins.git.UserRemoteConfig' {
+                      'url'('https://github.com/zslavanya01/login.git')
+                  }
+              }
+              'branches' {
+                  'hudson.plugins.git.BranchSpec' {
+                      'name'('*/main')
+                  }
+              }
+          }
+          'scriptPath'('Jenkinsfile')
+          'lightweight'(true)
+        }
+
+    }
+}
+
+pipelineJob('CI-Pipelines/todo-ci') {
+    configure { flowdefination ->
+      flowdefination << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+          'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+              'userRemoteConfigs' {
+                  'hudson.plugins.git.UserRemoteConfig' {
+                      'url'('https://github.com/zslavanya01/todo.git')
+                  }
+              }
+              'branches' {
+                  'hudson.plugins.git.BranchSpec' {
+                      'name'('*/main')
+                  }
+              }
+          }
+          'scriptPath'('Jenkinsfile')
+          'lightweight'(true)
+        }
+
+    }
+}
+
+pipelineJob('CI-Pipelines/users-ci') {
+    configure { flowdefination ->
+      flowdefination << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+          'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+              'userRemoteConfigs' {
+                  'hudson.plugins.git.UserRemoteConfig' {
+                      'url'('https://github.com/zslavanya01/users.git')
+                  }
+              }
+              'branches' {
+                  'hudson.plugins.git.BranchSpec' {
+                      'name'('*/main')
+                  }
+              }
+          }
+          'scriptPath'('Jenkinsfile')
+          'lightweight'(true)
+        }
+
+    }
+}
+
+pipelineJob('CI-Pipelines/redis-ci') {
+    configure { flowdefination ->
+      flowdefination << delegate. 'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps'){
+          'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+              'userRemoteConfigs' {
+                  'hudson.plugins.git.UserRemoteConfig' {
+                      'url'('https://github.com/zslavanya01/redis.git')
+                  }
+              }
+              'branches' {
+                  'hudson.plugins.git.BranchSpec' {
+                      'name'('*/main')
+                  }
+              }
+          }
+          'scriptPath'('Jenkinsfile')
+          'lightweight'(true)
+        }
+
+    }
+}
