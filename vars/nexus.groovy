1b5d575f31c7f25531303989767ff076928ffee3
zslavanya <lavanya.k@zelarsoft.com>
2021-06-03 11:53:00 +0530

    .

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -2,3 +2,7 @@ def nexus() {
     command = "curl -f -v -u admin:admin123 --upload-file /home/ubuntu/workspace/CI-Pipelines/frontend.zip http://${NEXUS_IP}:8081/repository/frontend/frontend1.zip"
     def execute_state=sh(returnStdout: true, script: command)
 }
+
+def demos() {
+    print 'Demo'
+}
