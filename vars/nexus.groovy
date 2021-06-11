0235122c7ef76d89f6a38cf59158f586d3c20969
zslavanya <lavanya.k@zelarsoft.com>
2021-06-11 16:16:21 +0530

    ,

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -4,6 +4,8 @@ def nexus() {
 }
 
 def make_artifacts(APP_TYPE,COMPONENT) {
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}'"
+    def get_branch_exec=sh(returnStdout: true, script: get_branch)
     if(APP_TYPE == "NGINX") {
         command = "zip -r ../${COMPONENT}.zip *"
         def execute_com=sh(returnStdout: true, script: command)
@@ -22,8 +24,7 @@ def make_artifacts(APP_TYPE,COMPONENT) {
 
 def code_build(APP_TYPE,COMPONENT) {
     if(APP_TYPE == "NODEJS") {
-        //command = "npm install"
-        command = "env"
+        //command = "npm install
         def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     } else if(APP_TYPE == "JAVA") {
