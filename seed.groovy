e137a7a82d9dfbfa12a4aec6576412f639a22819
zslavanya <lavanya.k@zelarsoft.com>
2021-06-11 14:59:46 +0530

    .

--- seed.groovy
+++ seed.groovy
@@ -22,7 +22,7 @@ for (i in 0..count) {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zsdevops01/'+j+'.git')
+              'url'('https://github.com/zslavanya01/'+j+'.git')
               'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
             }
           }
