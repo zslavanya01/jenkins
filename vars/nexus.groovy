b5ac378d280794932bed2766e4990a8e403f94ea
zslavanya <lavanya.k@zelarsoft.com>
2021-06-23 16:15:03 +0530

    .

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,20 +1,20 @@
 def nexus(COMPONENT) {
-    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}'"
+    get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
-
+    
     command = "curl -f -v -u admin:admin123 --upload-file ${FILENAME} http://172.31.13.117:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true, script: command)
+    manager.addShortText("deployed")
 }
 
 def make_artifacts(APP_TYPE, COMPONENT) {
     get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
-    println("abc${get_branch_exec}abc")
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
     if(APP_TYPE == "NGINX") {
-        command = "zip -r ${FILENAME} *"
-        def execute_com=sh(returnStdout: true, script: command)
+        command = "zip -r ${FILENAME} ."
+        def execute_com=sh(returnStdout:true, script: command)
         print execute_com
     } else if(APP_TYPE == "NODEJS") {
       command = "zip -r ${FILENAME} *"
