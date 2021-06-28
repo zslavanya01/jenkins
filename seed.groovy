063f5e4e5ad26057eadc5bf430f8b4898cfee337
zslavanya <lavanya.k@zelarsoft.com>
2021-06-28 13:00:22 +0530

    .

--- seed.groovy
+++ seed.groovy
@@ -1,62 +1,83 @@
-folder('CI-Pipelines') {
-  displayName('CI Pipelines')
-  description('CI Pipelines')
-}
+// folder('CI-Pipelines') {
+//   displayName('CI Pipelines')
+//   description('CI Pipelines')
+// }
 
-def component = ["frontend","login","users","todo","redis"];
+// def component = ["frontend","login","users","todo","redis"];
 
-def count=(component.size()-1)
-for (i in 0..count) {
-  def j=component[i]
-  pipelineJob("CI-Pipelines/${j}-ci") {
-    configure { flowdefinition ->
-      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-          'hudson.triggers.SCMTrigger' {
-            'spec'('* * * * 1-5')
-            'ignorePostCommitHooks'(false)
-          }
-        }
-      }
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/zslavanya01/'+j+'.git')
-              //'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
-}
+// def count=(component.size()-1)
+// for (i in 0..count) {
+//   def j=component[i]
+//   pipelineJob("CI-Pipelines/${j}-ci") {
+//     configure { flowdefinition ->
+//       flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+//         'triggers' {
+//           'hudson.triggers.SCMTrigger' {
+//             'spec'('* * * * 1-5')
+//             'ignorePostCommitHooks'(false)
+//           }
+//         }
+//       }
+//       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//           'userRemoteConfigs' {
+//             'hudson.plugins.git.UserRemoteConfig' {
+//               'url'('https://github.com/zslavanya01/'+j+'.git')
+//               //'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+//             }
+//           }
+//           'branches' {
+//             'hudson.plugins.git.BranchSpec' {
+//               'name'('*/tags/*')
+//             }
+//           }
+//         }
+//         'scriptPath'('Jenkinsfile')
+//         'lightweight'(true)
+//       }
+//     }
+//   }
+// }
 
-pipelineJob("Deployment Pipeline") {
+// pipelineJob("Deployment Pipeline") {
+//    configure { flowdefinition ->
+//      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//          'userRemoteConfigs' {
+//            'hudson.plugins.git.UserRemoteConfig' {
+//             'url'('https://github.com/zslavanya01/jenkins.git')
+//             'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+//           }
+//         }
+//          'branches' {
+//           'hudson.plugins.git.BranchSpec' {
+//              'name'('*/tags/*')
+//            }
+//          }
+//        }
+//       'scriptPath'('Jenkinsfile-Deployment')
+//        'lightweight'(true)
+//     }
+//   }
+// }
+
+pipelineJob('frontend-ci') {
    configure { flowdefinition ->
      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
          'userRemoteConfigs' {
            'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/zslavanya01/jenkins.git')
-            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+            'url'('https://github.com/zslavanya01/frontend.git')
           }
         }
-         'branches' {
-          'hudson.plugins.git.BranchSpec' {
-             'name'('*/tags/*')
-           }
-         }
-       }
+        'branches' {
+         'hudson.plugins.git.BranchSpec' {
+            'name'('*/tags/*')
+          }
+        }
+      }
       'scriptPath'('Jenkinsfile-Deployment')
-       'lightweight'(true)
+      'lightweight'(true)
     }
   }
 }
