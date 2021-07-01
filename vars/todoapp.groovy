8cf6a03b2c5145de6c951579c8572c976d25cc61
zslavanya <lavanya.k@zelarsoft.com>
2021-07-01 08:14:05 +0530

    .

--- vars/todoapp.groovy
+++ vars/todoapp.groovy
@@ -71,9 +71,9 @@ def call(Map params = [:]) {
         }
 
         triggers {
-            pollSCM('*/2 * * * 1-5')
+            pollSCM('*/2 * * 1-5')
         }
-        
+
         environment {
             COMPONENT    = "${args.COMPONENT}"
             NEXUS_IP     = "${args.NEXUS_IP}"
