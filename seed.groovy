904d084232ca2d40c2441fccc097d95ab11dad1b
zslavanya <lavanya.k@zelarsoft.com>
2021-07-16 13:26:39 +0530

    .

--- seed.groovy
+++ seed.groovy
@@ -104,3 +104,25 @@ for(i in 0..count) {
 
 }
 
+pipelineJob("Deployment Pipeline") {
+   configure { flowdefinition ->
+     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+         'userRemoteConfigs' {
+           'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/zslavanya01/jenkins.git')
+            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+          }
+        }
+         'branches' {
+          'hudson.plugins.git.BranchSpec' {
+             'name'('*/tags/*')
+           }
+         }
+       }
+      'scriptPath'('Jenkinsfile-Deployment')
+       'lightweight'(true)
+    }
+  }
+}
+
