954e760fe5b5cb439686cd94d900b7e12fc25cf3
zslavanya <lavanya.k@zelarsoft.com>
2021-08-02 13:26:44 +0530

    ,

---
+++ vars/docker.groovy
@@ -0,0 +1,36 @@
+def call(Map params = [:]) {
+    
+    pipeline {
+        agent {
+         label "${args.SLAVE_LABEL}"
+        }
+
+        triggers {
+            pollSCM('* * * * 1-5')
+        }
+        
+        environment {
+            COMPONENT    = "${args.COMPONENT}"
+            
+            PROJECT_NAME = "${args.PROJECT_NAME}"
+            SLAVE_LABEL  = "${args.SLAVE_LABEL}"
+            APP_TYPE     = "${args.APP_TYPE}"
+        }
+
+        stages {
+
+            stage('Build Code & Install Dependencies') {
+                steps {
+                    sh '''
+                       docker build -t local .
+                     
+                    ''' 
+                }
+            }
+
+
+
+        }
+
+    }
+}
